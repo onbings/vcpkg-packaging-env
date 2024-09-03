f6a0ad2621d55bb31077fc6e713ee6c1dfd27e9b
bha <b.harmel@evs.com>
2024-09-03 14:25:26 +0200

    add bofdearimgui and bofwebrpc

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <bofstd/bofstd.h>
-#include <bof2d/bof2d.h>
+#include <bofdearimgui/bof_dearimgui.h>
+#include <bofwebrpc/bofwebrpc.h>
 
 int main(int, char**)
 {
   printf("BofStd version %s\n", BOF::Bof_GetVersion().c_str());
-  printf("Bof2d  version %s\n", BOF2D::Bof_GetVersion().c_str());
  
   return 0;
 }
